d23dff5d68f5e78586d8953c606a9f949a0a5611
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2016-11-17 00:27:59 +0100

    Fix path to my code dir. dlontkov instead of qpython

--- localsubmission/submitSkeleton.sh
+++ localsubmission/submitSkeleton.sh
@@ -4,7 +4,7 @@
 #PBS -l walltime=00:45:00
 
 # setting up your code and your env
-cd /user/qpython/TopBrussels7X/CMSSW_7_6_3/src/TopBrussels/DisplacedTops
+cd /user/dlontkov/CMSSW_8_0_21/src/TopBrussels/DisplacedTop
 eval `scramv1 runtime -sh`
 
 # want you really want to do!!
--- localsubmission/submitTestSkeleton.sh
+++ localsubmission/submitTestSkeleton.sh
@@ -2,7 +2,7 @@
 #PBS -q express
 
 # setting up your code and your env
-cd /user/qpython/TopBrussels7X/CMSSW_7_6_3/src/TopBrussels/DisplacedTops
+cd /user/dlontkov/CMSSW_8_0_21/src/TopBrussels/DisplacedTop
 eval `scramv1 runtime -sh`
 
 # want you really want to do!!
